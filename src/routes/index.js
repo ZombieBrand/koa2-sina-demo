ebed5814284212a978b6c7faf3c8a7c7634b9a5e
ZombieBrand <zombiebrandg@gmail.com>
2020-08-03 17:56:51 +0800

    feat:路由的演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,28 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
+    title: "koa2 json",
+  };
+});
+
+router.get("/profile/:userName", async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    userName
+  };
+});
+router.get('/loadMore/:userName/:pageIndex', async (ctx,next)=>{
+  const {userName,pageIndex}=ctx.params
+  ctx.body={
+    userName,
+    pageIndex
   }
 })
-
-module.exports = router
+module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,20 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.prefix('/users')
+router.prefix("/users");
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.get("/", function (ctx, next) {
+  ctx.body = "this is a users response!";
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+router.get("/bar", function (ctx, next) {
+  ctx.body = "this is a users/bar response";
+});
 
-module.exports = router
+router.post("/login", async (ctx, next) => {
+  const { userName, passWord } = ctx.request.body;
+  ctx.body = {
+    userName,
+    passWord
+  }
+});
+module.exports = router;
