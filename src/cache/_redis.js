8e229b18dc6763ddf9e5d3a4f3ae56d2d1f890ee
ZombieBrand <zombiebrandg@gmail.com>
2020-08-11 15:22:51 +0800

    feat: redis操作

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -1,10 +1,6 @@
 /*
  * @Author: ZombieBrand
- * @Date: 2020-08-05 17:14:28
- * @LastEditTime: 2020-08-05 17:26:08
- * @LastEditors: Please set LastEditors
  * @Description: 连接redis的方法get set
- * @FilePath: \koa2-sina-demo\src\cache\_redis.js
  */
 
 const redis = require('redis')
@@ -12,3 +8,51 @@ const { REDIS_CONF } = require('../config/db')
 
 // 创建客户端
 const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+redisClient.on('error', err => {
+    console.log('redis error', err)
+})
+/**
+ * redis set
+ * @param {string} key 
+ * @param {string} val 
+ * @param {number} timeout 单位秒
+ */
+function set(key, val, timeout = 60 * 60) {
+    if (typeof val === 'object') {
+        val = JSON.stringify(val)
+    }
+    redisClient.set(key, val)
+    // 过期时间
+    redisClient.expire(key, timeout)
+}
+/**
+ * redis get
+ * @param {string} key 键 
+ */
+function get(key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            if (val === null) {
+                resolve(null)
+                return
+            }
+            try {
+                resolve(
+                    JSON.parse(val)
+                )
+            } catch (err) {
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
