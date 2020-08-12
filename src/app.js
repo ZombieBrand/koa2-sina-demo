c69c4451ab489a1c2a72b7de295af8bd405beaff
ZombieBrand <zombiebrandg@gmail.com>
2020-08-12 18:05:45 +0800

    feat:增加public前端显示代码

--- src/app.js
+++ src/app.js
@@ -50,12 +50,12 @@ app.use(
 );
 
 // logger
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
 app.use(index.routes(), index.allowedMethods());
